621cf32b968ff84e2f51c5460a2e0ffe5eb1e6dd
Suijin <70591379+Cyberkingcr7@users.noreply.github.com>
2021-11-14 06:30:23 +0200

    Update gen.ts

--- src/commands/Weeb/gen.ts
+++ src/commands/Weeb/gen.ts
@@ -12,7 +12,7 @@ export default class Command extends BaseCommand {
             command: 'genshincharacter',
             description: `Gives you the data of the given genshin character.`,
             aliases: ['gchar', 'genshin'],
-            category: 'anime',
+            category: 'weeb',
             usage: `${client.config.prefix}gchar [name]`,
             baseXp: 50
         })
