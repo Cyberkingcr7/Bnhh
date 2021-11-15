473da1cfda51c0cc9cd4889bfdbf38f1e2b246a6
Suijin <70591379+Cyberkingcr7@users.noreply.github.com>
2021-11-15 20:46:19 +0200

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -17,21 +17,10 @@ export default class Command extends BaseCommand {
 
    run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
             const n = [
-            './assets/images/help/help.mp4'
+             './assets/images/help/help.mp4'
         ]
         let bnh = n[Math.floor(Math.random() * n.length)]
-        if (!parsedArgs.joined) {
-            const commands = this.handler.commands.keys()
-            const categories: { [key: string]: ICommand[] } = {}
-            for (const command of commands) {
-                const info = this.handler.commands.get(command)
-                if (!command) continue
-                if (!info?.config?.category || info.config.category === 'dev') continue
-                if (Object.keys(categories).includes(info.config.category)) categories[info.config.category].push(info)
-                else {
-                    categories[info.config.category] = []
-                    categories[info.config.category].push(info)
-                }
+        if (!parsedArgs.joined
             }
             let text = ` 🌊hello! *${M.sender.username}*, I'm bnh.\n\nMy prefix is - "${this.client.config.prefix}"\n\nThe usable commands are listed below.\n\n`
             const keys = Object.keys(categories)
