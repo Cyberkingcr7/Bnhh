3ec8cc04c8431efc46b88f96507b6b27ec60db2b
Suijin <70591379+Cyberkingcr7@users.noreply.github.com>
2021-11-16 08:00:07 +0200

    Create media.ts

---
+++ src/commands/Media/media.ts
@@ -0,0 +1,48 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import { join } from 'path'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'media',
+            description: 'shows media section',
+            category: 'media',
+            usage: `${client.config.prefix}help (command_name)`,
+            dm: true,
+            aliases: ['5']
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        const n = [
+            './assets/images/help/help.mp4'
+        ]
+        let bnh = n[Math.floor(Math.random() * n.length)]
+        const mn=`
+Konnichiwa ${M.sender.username}
+    this is ${this.client.config.prefix}media
+ Shows the media info
+   
+        「 Media 」
+ -「 ${this.client.config.prefix}Blur 」
+ -「 ${this.client.config.prefix}google 」
+ -「 ${this.client.config.prefix}play 」
+ -「 ${this.client.config.prefix}retrieve 」
+ -「 ${this.client.config.prefix}re 」
+ -「 ${this.client.config.prefix}ship 」
+  -「 ${this.client.config.prefix}trigger 」
+ -「 ${this.client.config.prefix}advice 」
+ -「 ${this.client.config.prefix}jail 」
+ -「 ${this.client.config.prefix}why 」
+ -「 ${this.client.config.prefix}chess 」
+  *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*`
+        return void this.client.sendMessage(M.from, { url: bnh }, MessageType.video, {quoted:M.WAMessage,
+            mimetype: Mimetype.gif,
+            caption:mn }
+        )
+    }
+}
