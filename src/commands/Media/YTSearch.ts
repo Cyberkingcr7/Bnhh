e700ac67a474861599fd771c96becdb7dab7e700
Suijin <70591379+Cyberkingcr7@users.noreply.github.com>
2021-11-16 10:20:11 +0200

    Update YTSearch.ts

--- src/commands/Media/YTSearch.ts
+++ src/commands/Media/YTSearch.ts
@@ -29,14 +29,14 @@ export default class Command extends BaseCommand {
                 videos[i].author.name
             }\n 📙 *Duration:* ${videos[i].duration}\n📘 *URL:* ${videos[i].url}\n\n`
         }
-        M.reply('👾 searching...')
+        M.reply('「 searching for info...」')
         this.client
             .sendMessage(M.from, text, MessageType.extendedText, {
                 quoted: M.WAMessage,
                 contextInfo: {
                     externalAdReply: {
                         title: `Search Term: ${term}`,
-                        body: `👾Handcrafted for you by Kaoi👾`,
+                        body: `👾Handcrafted for you by bnh👾`,
                         mediaType: 2,
                         thumbnailUrl: videos[0].thumbnail,
                         mediaUrl: videos[0].url
