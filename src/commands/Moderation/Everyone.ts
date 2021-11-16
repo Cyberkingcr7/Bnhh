3debcd21dfb0e39ef937b0a4f0744e44858edbfa
Suijin <70591379+Cyberkingcr7@users.noreply.github.com>
2021-11-16 09:51:25 +0200

    Update Everyone.ts

--- src/commands/Moderation/Everyone.ts
+++ src/commands/Moderation/Everyone.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const term = joined.trim()
         return void (await M.reply(
-            `${M.groupMetadata?.subject || '*EVERYONE*'}\n*term*\n`,
+            `${M.groupMetadata?.subject || '*EVERYONE*'}\n*(term)*\n`,
             undefined,
             undefined,
             M.groupMetadata?.participants.map((user) => user.jid)
