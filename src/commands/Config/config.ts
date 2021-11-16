737a6adcc266c2a7780569d3f7efa36fa759bb2b
Suijin <70591379+Cyberkingcr7@users.noreply.github.com>
2021-11-16 06:59:43 +0200

    Create config.ts

---
+++ src/commands/Config/config.ts
@@ -0,0 +1,40 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import { join } from 'path'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'config',
+            description: 'shows bot section',
+            category: 'general',
+            usage: `${client.config.prefix}help (command_name)`,
+            dm: true,
+            aliases: ['2']
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        const n = [
+            './assets/images/help/help.mp4'
+        ]
+        let bnh = n[Math.floor(Math.random() * n.length)]
+        const mn=`
+Konnichiwa ${M.sender.username}
+    this is ${this.client.config.prefix}Config
+ Shows the config info
+   
+        「 Bot 」
+ -「 ${this.client.config.prefix}enable 」
+ -「 ${this.client.config.prefix}disable 」
+
+ *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*`
+        return void this.client.sendMessage(M.from, { url: bnh }, MessageType.video, {quoted:M.WAMessage,
+            mimetype: Mimetype.gif,
+            caption:mn }
+        )
+    }
+}
