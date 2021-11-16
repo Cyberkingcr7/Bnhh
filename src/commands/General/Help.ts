4ee5cb9d36d5df169d3d09ddf40645209ce41dc0
Suijin <70591379+Cyberkingcr7@users.noreply.github.com>
2021-11-16 05:16:55 +0200

    Update Help.ts
    Help list no longer auto generated

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -21,136 +21,29 @@ export default class Command extends BaseCommand {
         const n = [
             './assets/images/help/help.mp4'
         ]
-        let chitoge = n[Math.floor(Math.random() * n.length)]
+        let bnh = n[Math.floor(Math.random() * n.length)]
         const mn=`
-🪆𝐊𝐨𝐧𝐢𝐜𝐡𝐢𝐰𝐚! 𝐈'𝐦 *𝙸𝚌𝚑𝚒𝚐𝚘-𝙱𝚘𝚝𝚝𝚘* , 𝐞𝐧𝐣𝐨𝐲 𝐮𝐬𝐢𝐧𝐠 𝐦𝐲 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬..! 
-🎋 𝐇𝐞𝐫𝐞 𝐚𝐫𝐞 𝐭𝐡𝐞 𝐋𝐢𝐬𝐭𝐞𝐝 𝐂𝐨𝐦𝐦𝐚𝐧𝐝𝐬 : -
-
-🎊 𝐅𝐔𝐍 🎊
-
-🔖 ғᴀᴄᴛ  
-🔖 ǫᴜᴏᴛᴇ
-🔖 ғǫᴜᴏᴛᴇ      
-🔖 ᴡʜʏ
-🔖 ᴊᴏᴋᴇ        
-🔖 ᴛʀɪɢɢᴇʀ
-
-⛩️ 𝐖𝐄𝐄𝐁 ⛩️
-
-🏷️ᴡᴀɪғᴜ            
-🏷️ ɴᴇᴋᴏ
-🏷️ ᴀɴɪᴍᴇ-ʟɪɴᴇ      
-🏷️ ʟᴏʟɪ
-🏷️ ʀᴘᴀᴘᴇʀ          
-🏷️ ᴋɪᴛsᴜɴᴇ
-🏷️ ᴍᴇɢᴜᴍɪᴘᴀᴘᴇʀ
-🏷️ ʜᴜsʙᴀɴᴅʀᴏ
-🏷️ sʜɪɴᴏʙᴜ-ᴘᴀᴘᴇʀ
-🏷️ ɢᴇɴ𝐬ʜɪɴ ᴄʜᴀʀᴀᴄᴛᴇʀ
-🏷️ ᴀɴɪᴍᴇ ᴄʜᴀʀᴀᴄᴛᴇʀ
-
-🎐 𝐆𝐄𝐍𝐄𝐑𝐀𝐋 🎐
-
-♐ ᴀᴅᴍɪɴs        
-♐ ᴅᴇʟᴇᴛᴇ
-♐ ᴇᴠᴇʀʏᴏɴᴇ       
-♐ ᴍᴏᴅs
-♐ ᴘʀᴏғɪʟᴇ        
-♐ xᴘ
-♐ ɢᴏᴏɢʟᴇ
-♐ ɪɴᴠɪᴛᴇ ʟɪɴᴋ
-♐ ʀᴇᴠᴏᴋᴇ
-♐ ᴜʀʙᴀɴ ᴅɪᴄᴛɪᴏɴᴀʀʏ
-
-🎼 𝐌𝐄𝐃𝐈𝐀 🎼
-
-🎶 ʟʏʀɪᴄs     
-🎶 sᴘᴏᴛɪғʏ
-🎶 ʏᴛᴀ         
-🎶 ʏᴛᴠ
-🎶 ʏᴛs         
-🎶 ᴘʟᴀʏ
-🎶 ᴘʟᴀʏᴠ
-🎶 ssᴡᴇʙ
-
-📃 𝐌𝐈𝐒𝐂 📃
-
-⛓️ ʜɪ          
-⛓️ ʀᴇᴛʀɪᴇᴠᴇ
-⛓️ ᴄʀʏᴘᴛᴏ
-⛓️ ɢɪᴛʜᴜʙ  
-⛓️ ᴡᴇᴀᴛʜᴇʀ
-⛓️ ᴇʟᴇᴍᴇɴᴛ
-
-📯 𝐌𝐎𝐃𝐄𝐑𝐀𝐓𝐈𝐎𝐍 📯
-
-📍ᴀᴄᴛɪᴠᴀᴛᴇ     
-📍ᴄʟᴏsᴇ
-📍ᴅᴇᴀᴄᴛɪᴠᴀᴛᴇ   
-📍ᴏᴘᴇɴ
-📍ᴅᴇᴍᴏᴛᴇ        
-📍ᴘᴜʀɢᴇ
-📍ᴘʀᴏᴍᴏᴛᴇ       
-📍ʀᴇᴍᴏᴠᴇ
-📍ɢʀᴏᴜᴘ ᴄʜᴀɴɢᴇ
-
-⚠️ 𝐍𝐒𝐅𝐖 ⚠️
-
-🀄 ʙʟᴏᴡᴊᴏʙ    
-🀄 ɴsғᴡᴊᴏᴋᴇ
-🀄 ɴsғᴡ-ɴᴇᴋᴏ  
-🀄 ᴛʀᴀᴘ
-🀄 ɴsғᴡ-ᴡᴀɪғᴜ
-
-🎭 𝐑𝐄𝐀𝐂𝐓𝐈𝐎𝐍𝐒 🎭
-
-🎗️ ʙɪᴛᴇ        
-🎗️ ʜɪɢʜғɪᴠᴇ
-🎗️ ʙʟᴜsʜ       
-🎗️ sᴍᴜɢ
-🎗️ ʙᴏɴᴋ        
-🎗️ ʜᴜɢ
-🎗️ ʙᴜʟʟʏ       
-🎗️ ᴋɪᴄᴋ
-🎗️ ᴄʀɪɴɢᴇ      
-🎗️ ᴋɪʟʟ
-🎗️ ᴄʀʏ         
-🎗️ ᴋɪss
-🎗️ ᴄᴜᴅᴅʟᴇ      
-🎗️ ʟɪᴄᴋ
-🎗️ ᴅᴀɴᴄᴇ       
-🎗️ ᴘᴀᴛ
-🎗️ ᴇᴀᴛ         
-🎗️ ᴘᴏᴋᴇ
-🎗️ ʜᴀɴᴅʜᴏʟᴅ    
-🎗️ sʟᴀᴘ
-🎗️ ʜᴀᴘᴘʏ       
-🎗️ sᴍɪʟᴇ
-🎗️ ᴜʟᴛʀᴀʜᴜɢ    
-🎗️ ᴡᴀᴠᴇ
-🎗️ ᴡɪɴᴋ        
-🎗️ ʏᴇᴇᴛ
-
-🪦 𝐔𝐓𝐋𝐈𝐒 🪦
-
-⏳ ʙʟᴜʀ    
-⏳ sᴛɪᴄᴋᴇʀ
-⏳ sᴛᴇᴀʟ   
-⏳ sᴜʙʀᴇᴅ
-
-🖥️ 𝐃𝐄𝐕 🖥️
-
-💻 ʙᴀɴ
-💻 ᴅɪ𝐬ᴀʙʟᴇ
-💻 ᴇɴᴀʙʟᴇ
-💻 ᴇᴠᴀʟ
-💻 ʟᴇᴀᴠᴇ
-💻 sᴛᴀᴛᴜs
-💻 ᴜɴʙᴀɴ
+Konnichiwa *${M.sender.username}* im bnh 
+     My prefix is "${this.client.config.prefix}"
+ The usable commands are listed below
+   
+        「 Sections 」
+ -「 Bots 」
+ -「 Config 」
+ -「 Dev 」
+ -「 Fun && games 」
+ -「 Educative 」
+ -「 General 」
+ -「 Media 」
+ -「 Moderation 」
+ -「 Weeb 」
+ -「 Nsfw (18+) 」
  
-𓊈𒆜𝐈𝐜𝐡𝐢𝐠𝐨-𝐁𝐨𝐭𝐭𝐨𒆜𓊉
+ *Note: Use ${this.client.config.prefix}help <Section_name> to view the section info*
+     
+     
 `
-        return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {quoted:M.WAMessage,
+        return void this.client.sendMessage(M.from, { url: bnh }, MessageType.video, {quoted:M.WAMessage,
             mimetype: Mimetype.gif,
             caption:mn }
         )
