ff22cf88a264245e745454b0b3e21a8a076095b6
Suijin <70591379+Cyberkingcr7@users.noreply.github.com>
2022-02-12 20:28:47 +0200

    Update Chat.ts

--- src/commands/Fun/Chat.ts
+++ src/commands/Fun/Chat.ts
@@ -33,10 +33,10 @@ export default class Command extends BaseCommand {
                     return void M.reply(res.data.cnt)
                 })
                 .catch(() => {
-                    M.reply(`Intriguing...`)
+                    M.reply(`error...`)
                 })
         } else {
-            M.reply(`Chat Bot Url not set\nRefer to ${this.client.config.prefix}guide to get Chat Bot Url`)
+            M.reply(`Chat Bot Url not set.`)
         }
     }
 }
